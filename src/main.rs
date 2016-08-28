afce06fb2208a4ab1653904198fdb726b300f7b4
Slava Semushin <slava.semushin@gmail.com>
2016-08-28 22:49:21 +0200

    main.rs: add method tokens_from_string and test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[cfg(not(test))]
 fn main() {
     println!("Here will be a garden!");
+    tokens_from_string();
+}
+
+fn tokens_from_string() {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::tokens_from_string;
+
+    #[test]
+    fn should_handle_empty_string() {
+        tokens_from_string();
+    }
+
 }
