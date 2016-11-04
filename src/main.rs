php-coder/gtl

commit 8cb9b702e4e25573aa5ec829a4da015921e41fad

tokens_to_string(): don't take ownership of its argument.
No functional changes.

src/main.rs
@@ -56,7 +56,7 @@ fn text_to_tokens(text: &str) -> Vec<Token> {
     tokens
 }
 
-fn tokens_to_string(tokens: Vec<Token>) -> String {
+fn tokens_to_string(tokens: &Vec<Token>) -> String {
     tokens.iter()
         .map(|ref tok: &Token| tok.to_string())
         .collect::<Vec<_>>()
@@ -139,13 +139,13 @@ mod tests {
 
     #[test]
     fn tokens_to_string_returns_empty_string_for_empty_tokens() {
-        let tokens: Vec<Token> = Vec::new();
+        let ref tokens: Vec<Token> = Vec::new();
         assert_eq!(tokens_to_string(tokens), "");
     }
 
     #[test]
     fn tokens_to_string_returns_tokens_representation() {
-        let tokens: Vec<Token> = vec![Token::new("fn"),
+        let ref tokens: Vec<Token> = vec![Token::new("fn"),
                                       Token::new(" "),
                                       Token::new("main"),
                                       Token::new("("),

commit 86984fb8b8ef257ed462b9fc6c4961a8f5013bea

Unbreak build.
Broken by 8cb9b702e4e25573aa5ec829a4da015921e41fad commit.

src/main.rs
@@ -10,7 +10,8 @@ use std::io::{BufWriter, Write};
 
 #[cfg(not(test))]
 fn main() {
-    let mut code: String = tokens_to_string(text_to_tokens("create function main"));
+    let ref tokens: Vec<Token> = text_to_tokens("create function main");
+    let mut code: String = tokens_to_string(tokens);
     code.push_str("\n");
 
     // FIXME: from std::env::args() doc: "The returned iterator will panic